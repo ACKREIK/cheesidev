76d94694985b4e4ab4ad04e7cc92cf684bc41b8e
Aries Sky <jerifyemail@gmail.com>
2022-02-03 22:05:44 -0800

    amongus

---
+++ src/main/java/net/cheesidev/DEV.java
@@ -0,0 +1,12 @@
+package net.fabricmc.example;
+
+import java.fabricmc.api.ModInitializer;
+
+public class DEV implements ModInitializer {
+
+    @override
+    public void onInitialize() {
+
+    }
+
+}
